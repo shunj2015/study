05e83800243df578ff8e837f222b60e1eebea72d
shunj2015 <shunj2014@gmail.com>
2017-10-09 18:36:23 -0400

    123

--- build.sbt
+++ build.sbt
@@ -5,3 +5,8 @@ version := "0.1"
 scalaVersion := "2.11.8"
 
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
+
+assemblyMergeStrategy in assembly := {
+    case m if m.startsWith("META-INF") => MergeStrategy.discard
+    case _ => MergeStrategy.first
+}
--- src/main/scala/CollectionParallelize.scala
+++ src/main/scala/CollectionParallelize.scala
@@ -2,7 +2,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object CollectionParallelize {
     def main(args:Array[String]): Unit = {
-        val conf = new SparkConf().setAppName("cp").setMaster("master")
+        val conf = new SparkConf().setAppName("cp")
         val sc = new SparkContext(conf)
         val data = Array(1,2,3,4,5)
         val disdata = sc.parallelize(data)
