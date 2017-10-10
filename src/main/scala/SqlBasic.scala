bdc5e4addfe3d19030dcfab5b5fd1a546ef98808
Shun Jiang <shunjiang@sJiang-mbp15.local>
2017-10-10 14:09:59 -0700

    asdasd

--- src/main/scala/SqlBasic.scala
+++ src/main/scala/SqlBasic.scala
@@ -24,5 +24,19 @@ object SqlBasic {
     df.createGlobalTempView("gpeople")
     spark.sql("select * from global_temp.gpeople").show()
     spark.newSession().sql("select * from global_temp.gpeople").show()
+
+
+
+    println("---------datasets--------")
+
+    case class Person(name:String, age:Long)
+
+    val ds = Seq(Person("jim", "18")).toDS()
+    ds.show()
+    val ds2 = Seq(1,2,3).toDS().map(_+1).collect()
+
+    val dsp = spark.read.json("/home/shunj/xad/study/resources/sql/people.json").as[Person]
+    dsp.show()
+
   }
 }
