0df8d2b319795d2f31eca45a09bbe5724e174c05
Shun Jiang <shunjiang@sJiang-mbp15.local>
2017-10-10 15:09:49 -0700

    asdasd

--- src/main/scala/SqlSchemaRefl.scala
+++ src/main/scala/SqlSchemaRefl.scala
@@ -3,17 +3,25 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object SqlSchemaRefl {
   case class SegmentPair(name:String, segs:Array[Long])
+  def segmentSum(s: Seq[Long]): Long ={
+    var c = 0
+    s.foreach(c += _)
+    c
+  }
   def main(args:Array[String]): Unit = {
     val spark = SparkSession.builder().appName("sqlschemrfl").getOrCreate()
     import spark.implicits._
 
     val df = spark.sparkContext
       .textFile("/home/shunj/xad/study/resources/sql/segments.txt")
-      .map(_.split(" ")).map(atr=>SegmentPair(atr(0), atr(1).split(",").map(_.trim.toInt))).toDF()
+      .map(_.split(" ")).map(atr=>SegmentPair(atr(0), atr(1).split(",").map(_.trim.toLong))).toDF()
+
+    spark.sqlContext.udf.register("ssum", segmentSum _)
 
     df.createOrReplaceTempView("segment")
 
-    spark.sql("select * from segment").show()
+
+    spark.sql("select name, segmentSum(segs) from segment").show()
   }
 
 }
