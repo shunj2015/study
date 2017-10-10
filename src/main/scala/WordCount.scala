600f13f8ac9d44520a205b5943380fd1a9a77748
Shun Jiang <shunjiang@sJiang-mbp15.local>
2017-10-09 17:11:22 -0700

    asdasd

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -1,8 +1,14 @@
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
 
 object WordCount {
     def main(args: Array[String]): Unit = {
-
-        val sc = new SparkContext()
+        val conf = new SparkConf().setAppName("wordCount")
+        val sc = new SparkContext(conf)
+        val distFile = sc.textFile("/home/shunj/xad/study/resources/wordCount")
+        val counts = distFile
+          .flatMap(line => line.split(" "))
+          .map(word=>(word,1)).reduceByKey((a,b)=>a+b)
+        System.out.println(counts.collect())
+        counts.saveAsTextFile("/home/shunj/xad/study/resources/wordCount/output")
     }
 }
