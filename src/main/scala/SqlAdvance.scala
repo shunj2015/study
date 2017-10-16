090a2b96cc9db2b330fe6c562e8dab520e501319
Shun Jiang <shunjiang@sJiang-mbp15.local>
2017-10-16 16:09:55 -0700

    adasd

--- src/main/scala/SqlAdvance.scala
+++ src/main/scala/SqlAdvance.scala
@@ -9,6 +9,8 @@ object SqlAdvance {
     val userDF = spark.read.load("/usr/local/spark/examples/src/main/resources/users.parquet")
     userDF.select("favorite_color", "name").write.partitionBy("favorite_color").format("parquet").save("111.parquet")
 
+    spark.read.parquet("/home/shunj/xad/study/111.parquet").show()
+
   }
 }
 
